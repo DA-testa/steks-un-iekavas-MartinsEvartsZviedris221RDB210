fb682c5bbfddd9fb06ed89beb78b5b31b4dcfd85
Mārtiņš Evarts Zviedris <125508859+MartinsEvartsZviedris221RDB210@users.noreply.github.com>
2023-03-05 17:53:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,34 @@
-import sys
-import threading
+# python3
 
+from collections import namedtuple
 
-def compute_height(n, parents):
+Bracket = namedtuple("Bracket", ["char", "position"])
 
-    koks = [[] for _ in range(n)]
-    root_node = None
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
 
-    for child_node, parent_node in enumerate(parents):
-        
-        if parent_node == -1:
-            root_node = child_node
-            
-        else:
-            koks[parent_node].append(child_node)
-
-    def garums(node):
-        
-        height = 1
-        
-        if not koks[node]:
-            
-            return height
-        
-        else:
-            for child in koks[node]:
-                
-                height = max(height, garums(child))
-
-            return height + 1
-        
-    return garums(root_node)
-
+def find_mismatch(text):
+    text = input()
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i + 1))    
+        if next in ")]}":
+            # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack.pop().char,next):
+                return i+ 1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    return  "Success"
 
 def main():
+    text = input()
+    if text[0] == "I":
+        text
+        mismatch = find_mismatch(text)
+# Printing answer, write your code here
+    print(mismatch)
     
-    input_type = input("Ievadiet 'I' ja vēlaties skaitļus ievadīt no tastatūras vai 'F' ja vēlaties no faila ievadīt: ")
-
-    if input_type == "I":
-        
-        n = int(input("ievadiet 'nodes' daudzumu: "))
-        parents = list(map(int, input("ievadiet vērtības atdalot ar atstarpi: ").split()))
-       
-    elif input_type == "F":
-        
-        file_name = input("ievadiet faila nosaukumu: ")
-        
-        if "a" in file_name:
-            
-            print("faila nosaukums nedr;ikst saturēt burtu 'a'")
-            return
-        
-        try:
-            
-            with open(f"./test/{file_name}", "r") as f:
-                n = int(f.readline())
-                parents = list(map(int, f.readline().split()))
-                
-        except Exception as e:
-            print(f"Error: {e}")
-            return
-        
-    else:
-        print("Error: Invalid input type.")
-        return
-
-    print(compute_height(n, parents))
-
-
-# Increase recursion depth limit and stack size for larger inputs.
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
-threading.Thread(target=main).start()
+if __name__ == "__main__":
+    main()
